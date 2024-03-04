b98475b893ab9abcd588b8fedafe820e6f9859a3
regina <usmanova.re@yandex.ru>
2024-03-04 22:49:17 +0500

    Работа с разделом звуков
    Выполняется аналогично разделю медитации
    ❀ Дополнительное пояснение
    1. Создается класс NatureSounds, который наследуется от класса Fragment
    2. В методе onViewCreated() происходит настройка инициализации фрагмента
    3. Кнопке с id buttonSoothingSoundsOfRain устанавливается слушатель onClick, который при нажатии открывает ссылку на звуки дождя в видео на YouTube
    4. В методе onCreateView() раздувается макет для этого фрагмента meditation_fragment
    5. В компаньон объекте оставляется остальной код без изменений
    
    Команды используются для управления поведением фрагмента, инициализации элементов и отображения контента при запуске фрагмента
    Раздувание макета для этого фрагмента может быть необходимо для визуального представления текущего состояния фрагмента, его элементов и расположения на экране. Компаньон объект может быть нужен для работы с данными или дополнительной функциональностью фрагмента
    
    В строчке "val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))" происходит создание объекта Intent, который предназначен для выполнения определенного действия
    Каждое слово в этой команде играет свою роль:
    ➤ val: указывает на создание неизменяемой переменной
    ➤ intent: название переменной, которая будет содержать объект Intent
    ➤ =: оператор присваивания, который связывает переменную с результатом выражения
    ➤ Intent: класс, используемый для создания намерений (интентов)
    ➤ (Intent.ACTION_VIEW, Uri.parse(url)): это параметры конструктора Intent, где Intent.ACTION_VIEW указывает на действие, которое будет выполнено (в данном случае, просмотр), а Uri.parse(url) представляет URL, который будет открыт при выполнении этого действия

--- app/src/main/java/com/example/mindfulmoments/NatureSounds.kt
+++ app/src/main/java/com/example/mindfulmoments/NatureSounds.kt
@@ -1,14 +1,46 @@
 package com.example.mindfulmoments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class NatureSounds : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.buttonSoothingSoundsOfRain).setOnClickListener{
+            val url="https://www.youtube.com/watch?v=Zhq_ThHG2gA"
+            val intent =Intent(Intent.ACTION_VIEW,Uri.parse(url))
+            startActivity(intent)
+        }
+        view.findViewById<Button>(R.id.buttonTheEnticingSoundOfTheForest).setOnClickListener {
+            val url="https://www.youtube.com/watch?v=DIEY2DIzJgs"
+            val intent =Intent(Intent.ACTION_VIEW,Uri.parse(url))
+            startActivity(intent)
+        }
+        view.findViewById<Button>(R.id.buttonAlluringSoundOfTheOcean).setOnClickListener {
+            val url="https://www.youtube.com/watch?v=qTv8lRXM6Tw"
+            val intent =Intent(Intent.ACTION_VIEW,Uri.parse(url))
+            startActivity(intent)
+        }
+        view.findViewById<Button>(R.id.buttonSoundsOfASummerFire).setOnClickListener {
+            val url="https://www.youtube.com/watch?v=7NrEbQtQiKk"
+            val intent =Intent(Intent.ACTION_VIEW,Uri.parse(url))
+            startActivity(intent)
+        }
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Раздуваем макет для этого фрагмента
+        return inflater.inflate(R.layout.nature_sounds_fragment, container, false)
+    }
+
+    companion object {
+        // Остальной код оставляем без изменений
     }
 }
