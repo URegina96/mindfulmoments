272f979e7ef3d8d40af7c5ad0f03ca4b37a32a35
regina <usmanova.re@yandex.ru>
2024-03-04 21:22:02 +0500

    Работа с активностью и фрагментами
    Офрмление и подключение фрагмента
    ⟶ Звуки природы для релаксации и улучшения концентрации
    ⟶ Дневник благодарности для записи положительных моментов
    ⟶ Каталог с различными видами медитаций, дыхательных упражнений и практик осознанности
    Настройка напоминаний для практики осознанности в течение дня

--- app/src/main/java/com/example/mindfulmoments/MainActivity.kt
+++ app/src/main/java/com/example/mindfulmoments/MainActivity.kt
@@ -2,17 +2,40 @@ package com.example.mindfulmoments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val imageViewRegistration = findViewById<ImageView>(R.id.imageViewRegistration)
 
+        val imageViewRegistration = findViewById<ImageView>(R.id.imageViewRegistration)
         imageViewRegistration.setOnClickListener {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainerRegistration, Registration())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        val buttonMeditation=findViewById<Button>(R.id.buttonMeditation)
+        buttonMeditation.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerMeditation,Meditation())
+                .addToBackStack(null)
+                .commit()
+        }
+        val buttonNatureSounds=findViewById<Button>(R.id.buttonNatureSounds)
+        buttonNatureSounds.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerNatureSounds,NatureSounds())
+                .addToBackStack(null)
+                .commit()
+        }
+        val buttonGratitudeJournal=findViewById<Button>(R.id.buttonGratitudeJournal)
+        buttonGratitudeJournal.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerGratitudeJournal,GratitudeJournal())
                 .addToBackStack(null)
                 .commit()
         }
