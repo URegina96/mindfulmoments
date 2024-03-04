8839bf2102d5f54567cc51be93345d8ad43f8cf5
regina <usmanova.re@yandex.ru>
2024-03-04 15:37:13 +0500

    Работа с активностью и фрагментом  "Регистрация и вход"
    ❀ Для открытия фрагмента "Регистрация и вход" при нажатии на ImageView из активности MainActivity,  нужно добавить обработчик нажатия на ImageView и использовать фрагментный менеджер для замены текущего содержимого фрагментом "Регистрация и вход".
    Вот как это можно сделать:
    1. В файле MainActivity.kt добавила обработчик нажатия на ImageView и замену содержимого фрагмента
    2. В макете activity_main.xml, обеспечьте наличие контейнера для отображения фрагментов
    
    ❀ Дополнительная информация
    Разберем код построчно:
    
    🠞 `val imageViewRegistration = findViewById<ImageView>(R.id.imageViewRegistration)`: Здесь создается переменная `imageViewRegistration`, которая ссылается на ImageView с идентификатором `R.id.imageViewRegistration`. Метод `findViewById` используется для поиска и получения ссылки на виджет по его идентификатору
    🠞 `imageViewRegistration.setOnClickListener { ... }`: Здесь устанавливается слушатель кликов для `imageViewRegistration`. При клике на `imageViewRegistration` будет выполнен код внутри фигурных скобок
    🠞 `supportFragmentManager.beginTransaction()`: Здесь начинается транзакция фрагмента с помощью `supportFragmentManager`. Транзакция используется для добавления, удаления или замены фрагментов в активности
    🠞 `.replace(R.id.fragmentContainerRegistration, Registration())`: Здесь указывается, что в контейнер фрагментов с идентификатором `R.id.fragmentContainerRegistration` необходимо заменить текущий фрагмент на новый экземпляр `Registration`
    🠞 `.addToBackStack(null)`: Здесь добавляется транзакция в стек возврата, чтобы пользователь мог вернуться к предыдущему состоянию фрагмента при нажатии кнопки "назад"
    🠞 `.commit()`: Здесь происходит фактическое применение транзакции. Метод `commit()` применяет все изменения, сделанные в транзакции
    Таким образом, данный код устанавливает слушатель кликов на ImageView `imageViewRegistration`, который при клике заменяет содержимое контейнера фрагментов на новый фрагмент `Registration` с помощью транзакции фрагмента

--- app/src/main/java/com/example/mindfulmoments/MainActivity.kt
+++ app/src/main/java/com/example/mindfulmoments/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.mindfulmoments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageViewRegistration = findViewById<ImageView>(R.id.imageViewRegistration)
+
+        imageViewRegistration.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerRegistration, Registration())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
