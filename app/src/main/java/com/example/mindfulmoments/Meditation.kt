6d137daba3d5385ecc1a78de9c9a755ad1d1336f
regina <usmanova.re@yandex.ru>
2024-03-04 22:04:54 +0500

    Работа с разделом медитаций
    Для добавления функционала, который позволит пользователю переходить по ссылке на YouTube при нажатии на кнопку,  нужно сначала добавить обработчик события нажатия на кнопку, который будет открывать YouTube ссылку
    
    Как это можно сделать:
    
    ❀ В файле разметки (XML) добавить id для кнопки, чтобы можно было на нее ссылаться
    
    ❀ В файле фрагмента (Meditation.kt) добавить обработчик события нажатия на кнопку и открывание  YouTube ссылки

--- app/src/main/java/com/example/mindfulmoments/Meditation.kt
+++ app/src/main/java/com/example/mindfulmoments/Meditation.kt
@@ -1,15 +1,41 @@
 package com.example.mindfulmoments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class Meditation : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        view.findViewById<Button>(R.id.buttomMindfulBreathingMeditation).setOnClickListener{
+            val url="https://www.youtube.com/watch?v=AeMg5dNSM58"
+            val intent =Intent(Intent.ACTION_VIEW,Uri.parse(url))
+            startActivity(intent)
+        }
+        view.findViewById<Button>(R.id.buttomBodyScanMeditation).setOnClickListener {
+            val url ="https://www.youtube.com/watch?v=d21O7Al50WI"
+            val intent=Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+        view.findViewById<Button>(R.id.buttonLovingKindnessEditation).setOnClickListener {
+            val url ="https://www.youtube.com/watch?v=nsGnb5bL0nA"
+            val intent=Intent(Intent.ACTION_VIEW,Uri.parse(url))
+            startActivity(intent)
+        }
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Раздуваем макет для этого фрагмента
+        return inflater.inflate(R.layout.meditation_fragment, container, false)
+    }
+
+    companion object {
+        // Остальной код оставляем без изменений
     }
 }
